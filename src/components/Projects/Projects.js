0cfa598815a5e4b764d1df0a97fd48756d4bdbd6
ryliespriggs <rspriggs812@gmail.com>
2021-12-10 13:50:58 -0800

    added links to github repos

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
-
-
-
+//cannot get react-mdl to work
 
 class Projects extends Component {
     render() {
         return (
-            <div className='card'>
-                <div>
+            <div className='card-index'>
+                <div className="card">
                     CLI
+                    <br>
+                    </br>
+                    <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
                 </div>
-                <div>
-                    CLI
+                <br>
+                </br>
+                <br>
+                </br>
+                <div className="card">
+                    Sinatra
+                    <br>
+                    </br>
+                    <Button href="https://github.com/Rspriggs2020/sinatra_coffee_project" colored target="_blank">To Repo</Button>
                 </div>
-                <div>
-                    CLI
+                <br>
+                </br>
+                <br>
+                </br>
+                <div className="card">
+                    Rails
+                   <br>
+                   </br>
+                   <Button href="https://github.com/Rspriggs2020/hey_anime" colored target="_blank">To Repo</Button>
                 </div>
-                <div>
-                    CLI
+                <br>
+                </br>
+                <br>
+                </br>
+                <div className="card">
+                    Javascript 
+                    <br>
+                    </br>
+                    <Button href="https://github.com/Rspriggs2020/spacedout_frontend" colored target="_blank">To Repo</Button>
                 </div>
-                <div>
-                    CLI
+                <br>
+                </br>
+                <br>
+                </br>
+                <div className="card">
+                    React
+                    <br>
+                    </br>
+                    <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
                 </div>
-                   
-                 
+                    
             </div>
         )
 
