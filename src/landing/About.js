b07a7c06b405e9efefa3f9cd97eb3826736c98d8
ryliespriggs <rspriggs812@gmail.com>
2021-12-09 21:25:33 -0800

    adding home and footer to landing page

---
+++ src/landing/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className="signature">
+            <h3>&copy; 2021 MyReactProject</h3>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/landing/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="Homepage">
+           <p>Home</p>
+       </div>
+    )
+}
+
+export default Home;
