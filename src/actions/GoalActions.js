951f21b2a254833592885728297792831a136568
ryliespriggs <rspriggs812@gmail.com>
2021-12-10 10:13:35 -0800

    starting goal action and goal form

---
+++ src/actions/GoalActions.js
@@ -0,0 +1,21 @@
+export const getGoal = () => {
+
+}
+//get goal
+
+export const getGoalId = id => {
+
+}
+//get id
+
+export const createGoal = () => {
+
+}
+
+//create goal
+
+export const deleteGoal = () => {
+    
+}
+
+//delete goalwith id
--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
 class GoalForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            title: '',
+            description: ''
+        }
+    }
+
+    handleChange = event => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     render(){
 
     }
