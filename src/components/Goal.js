5700069c06787e2ec21a447ce7cb932064ac93f7
ryliespriggs <rspriggs812@gmail.com>
2021-12-10 10:47:23 -0800

    rough draft of goal js

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteGoal } from '../actions/GoalActions'
 
 class Goal extends Component {
+
+    handleClick() {
+        this.props.deleteGoal(this.props.goal.id)
+    }
+
     render() {
+
+        const { goal}  = this.props
+        return (
+            <div>
+                <h1>Goal: {goal.title}</h1>
+                <br>
+                Description: {goal.description}
+                <br>
+                </br>
+                <Button onClick={ () => this.handleClick() }>Delete Goal</Button>
+            </div>
+
+        )
 
     }
 }
 
-export default Goal;
+export default connect(null, { deleteGoal})(Goal);
