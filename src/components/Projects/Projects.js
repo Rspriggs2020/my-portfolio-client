8371d37b3cf816978831e4ecf36cdc5aa43a8b85
ryliespriggs <rspriggs812@gmail.com>
2021-12-10 14:58:04 -0800

    forgot to push, attempting to add goals to projects through conditionals

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Button } from 'react-bootstrap';
+import {Tabs, Tab} from 'react-bootstrap-tabs';
 import GoalsContainer from '../../containers/GoalContainer'
 
 //cannot get react-mdl to work
@@ -8,73 +9,85 @@ import GoalsContainer from '../../containers/GoalContainer'
 class Projects extends Component {
     constructor(props){
         super(props);
-        this.state = {
+        this.statestate = {
             browserTab: 0
         }
     }
 
-    toggleCategories() {
-        if(this.state.browserTab === 0)
-        return (
-            <div>
-                <GoalsContainer />
+    toggleCategories(){
+        if(this.state.browserTab === 0){
+          return(
+            <div >
+              <GoalsContainer />
             </div>
-        )
-    }
-    render() {
-        return (
-            <div className='card-index'>
+          )
+        }else if(this.state.browserTab === 1){
+          return (
+              <div className="project-card-1">
                 <div className="card">
                     CLI
                     <br>
                     </br>
                     <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
                 </div>
-                <br>
-                </br>
-                <br>
-                </br>
-                <div className="card">
-                    Sinatra
-                    <br>
-                    </br>
-                    <Button href="https://github.com/Rspriggs2020/sinatra_coffee_project" colored target="_blank">To Repo</Button>
+              </div>
+            )
+        }else if(this.state.browserTab === 2){
+            return (
+                <div className="project-card-2">
+                  <div className="card">
+                      CLI
+                      <br>
+                      </br>
+                      <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
+                  </div>
                 </div>
-                <br>
-                </br>
-                <br>
-                </br>
-                <div className="card">
-                    Rails
-                   <br>
-                   </br>
-                   <Button href="https://github.com/Rspriggs2020/hey_anime" colored target="_blank">To Repo</Button>
+            )
+        }else if(this.state.browserTab === 3){
+            return (
+                <div className="project-card-3">
+                  <div className="card">
+                      CLI
+                      <br>
+                      </br>
+                      <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
+                  </div>
                 </div>
-                <br>
-                </br>
-                <br>
-                </br>
-                <div className="card">
-                    Javascript 
-                    <br>
-                    </br>
-                    <Button href="https://github.com/Rspriggs2020/spacedout_frontend" colored target="_blank">To Repo</Button>
+            )
+        }else if(this.state.browserTab === 4){
+            return (
+                <div className="project-card-4">
+                  <div className="card">
+                      CLI
+                      <br>
+                      </br>
+                      <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
+                  </div>
                 </div>
-                <br>
-                </br>
-                <br>
-                </br>
-                <div className="card">
-                    React
-                    <br>
-                    </br>
-                    <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
+            )
+        }else if(this.state.browserTab === 5){
+            return (
+                <div className="project-card-5">
+                  <div className="card">
+                      CLI
+                      <br>
+                      </br>
+                      <Button href="https://github.com/Rspriggs2020/final_fantasy_cli" colored target="_blank">To Repo</Button>
+                  </div>
                 </div>
-                    
+            )
+        }
+    }
+
+    render(){
+        return(
+            <div className="display-projects">
+                
+                <p>project</p>
             </div>
         )
-
     }
+    
 }
 
 export default Projects;
