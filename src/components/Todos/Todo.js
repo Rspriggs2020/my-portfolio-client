5095b1beb284c1d6b79947d91ea415c18fdaf72b
ryliespriggs <rspriggs812@gmail.com>
2021-12-11 11:22:31 -0800

    rough layout of todo card

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button, Card } from 'semantic-ui-react';
 
-const Todo = () => {
+const Todo = ({ todo, deleteTodo, goal }) => {
+    return(
+        <Card>
+            <Card.Content>
+                <Card.Description>
+                {todo.title}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content>
+                <Button onClick={() => deleteTodo(todo, goal)}>Delete Todo</Button>
+            </Card.Content>
+        </Card>
 
+    )
 }
 
 export default Todo;
