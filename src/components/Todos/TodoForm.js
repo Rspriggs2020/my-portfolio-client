ea09003190e901169441b86dfe8e1e4ad79c2b12
ryliespriggs <rspriggs812@gmail.com>
2021-12-11 11:25:34 -0800

    intial code for the todo form

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from '../../actions/TodoActions'
 
 class TodoForm extends Component {
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            description: ''
+        }
+    }
 
+    handleChange = () => {
+
+    }
+
+    handleSubmit = () => {
+
+    }
+    
     render() {
 
     }
