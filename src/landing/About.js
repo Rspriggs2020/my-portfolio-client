b5d577053797c96ffca1353e94c04ee57d2e15a2
ryliespriggs <rspriggs812@gmail.com>
2021-12-11 17:11:41 -0800

    wrote my about page

--- src/landing/About.js
+++ src/landing/About.js
@@ -3,7 +3,16 @@ import React from "react";
 const About = () => {
     return(
         <div className="About">
-            <p>aboot page</p>
+            
+            <p>
+            In 2018 I abandoned everything I knew. I moved from Idaho to Seattle, WA. Although not a far move, the change of scenery, atmosphere, and different faces proved to be the change I desperately needed. The path I created for myself has been challenging, most times crazy but also incredibly rewarding. I came here to expand my education, however I took a detour and managed a coffee shop in Downtown Seattle for two years. I developed skills, relationships, and the confidence I needed to be able to take on the world, but before I did that, I needed to tackle school first. That was my journey that led me to Flatiron.
+
+Now that I am submitting my final project, I tend to spend a lot of time looking back at where I came from and who I was before the program. Never in my wildest dreams would I have ever iimagined that I would be where I am today. I am incredibly proud of myself for making this life my own.
+
+The driving force behind my motivation and dedication to do better for myself and by myself is the one person who means the most to me, and that is my wonderful mother. Everything I have done to get here is inspired by her and for her. With that, I hope to show the most impactful pieces of what makes me who I am.
+
+            </p>
+            
         </div>
     )
 }
