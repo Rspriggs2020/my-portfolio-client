ce2f0550b7cd50e0d25f618d7b6ff15a2d1c67af
ryliespriggs <rspriggs812@gmail.com>
2021-12-12 16:13:37 -0800

    more css

--- src/landing/Home.js
+++ src/landing/Home.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const Home = () => {
     return (
+        <body>
         <div className="Homepage">
-           
+           <div className="home-info">
+               <p style={{align: 'center', fontSize:'30px', margin: 'auto'}}> Hello there! My name is Rylie Spriggs and this is my portfolio for my final project of Flatiron
+               school. While attending Flatiron I've gained a tremendous amount of skills and knowledge of programming languages.
+                <br></br>
+                I've included links to each project and their relating blog posts. Also my goals to expand on those projects!
+                <br></br>
+                Cheers Flatiron!
+           </p>
+           </div>
        </div>
+       </body>
     )
 }
 
